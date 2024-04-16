6bd2ccfd11822d2d4ed431c02aa8c5511531ac60
tancheesen123 <tancheesen123@hotmail.com>
2024-04-16 21:56:37 +0800

    WiseWork

---
+++ lib/theme/app_decoration.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import '../core/app_export.dart';
+
+class AppDecoration {
+  // Fill decorations
+  static BoxDecoration get fillGray => BoxDecoration(
+        color: appTheme.gray50,
+      );
+  static BoxDecoration get fillOnErrorContainer => BoxDecoration(
+        color: theme.colorScheme.onErrorContainer,
+      );
+// Outline decorations
+  static BoxDecoration get outlineBlack => BoxDecoration(
+        color: theme.colorScheme.onErrorContainer,
+        boxShadow: [
+          BoxShadow(
+            color: appTheme.black900.withOpacity(0.02),
+            spreadRadius: 2.h,
+            blurRadius: 2.h,
+            offset: Offset(
+              0,
+              4,
+            ),
+          )
+        ],
+      );
+  static BoxDecoration get outlineBlack900 => BoxDecoration(
+        color: theme.colorScheme.onErrorContainer,
+        boxShadow: [
+          BoxShadow(
+            color: appTheme.black900.withOpacity(0.1),
+            spreadRadius: 2.h,
+            blurRadius: 2.h,
+            offset: Offset(
+              0,
+              10,
+            ),
+          )
+        ],
+      );
+  static BoxDecoration get outlineErrorContainer => BoxDecoration(
+        color: theme.colorScheme.onErrorContainer,
+        boxShadow: [
+          BoxShadow(
+            color: theme.colorScheme.errorContainer,
+            spreadRadius: 2.h,
+            blurRadius: 2.h,
+            offset: Offset(
+              0,
+              10,
+            ),
+          )
+        ],
+      );
+  static BoxDecoration get outlineGrayC => BoxDecoration(
+        color: theme.colorScheme.onErrorContainer,
+        border: Border.all(
+          color: appTheme.gray6004c,
+          width: 1.h,
+        ),
+      );
+  static BoxDecoration get outlinePrimary => BoxDecoration(
+        color: theme.colorScheme.onErrorContainer,
+        border: Border.all(
+          color: theme.colorScheme.primary,
+          width: 1.h,
+        ),
+      );
+}
+
+class BorderRadiusStyle {
+  // Rounded borders
+  static BorderRadius get roundedBorder12 => BorderRadius.circular(
+        12.h,
+      );
+  static BorderRadius get roundedBorder16 => BorderRadius.circular(
+        16.h,
+      );
+  static BorderRadius get roundedBorder20 => BorderRadius.circular(
+        20.h,
+      );
+}
---
+++ lib/widgets/custom_icon_button.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import '../core/app_export.dart';
+
+extension IconButtonStyleHelper on CustomIconButton {
+  static BoxDecoration get fillBlue => BoxDecoration(
+        color: appTheme.blue50,
+        borderRadius: BorderRadius.circular(20.h),
+      );
+  static BoxDecoration get fillIndigo => BoxDecoration(
+        color: appTheme.indigo500,
+        borderRadius: BorderRadius.circular(20.h),
+      );
+  static BoxDecoration get fillPrimary => BoxDecoration(
+        color: theme.colorScheme.primary,
+        borderRadius: BorderRadius.circular(15.h),
+      );
+  static BoxDecoration get fillGray => BoxDecoration(
+        color: appTheme.gray300,
+        borderRadius: BorderRadius.circular(15.h),
+      );
+}
+
+class CustomIconButton extends StatelessWidget {
+  CustomIconButton(
+      {Key? key,
+      this.alignment,
+      this.height,
+      this.width,
+      this.padding,
+      this.decoration,
+      this.child,
+      this.onTap})
+      : super(
+          key: key,
+        );
+
+  final Alignment? alignment;
+
+  final double? height;
+
+  final double? width;
+
+  final EdgeInsetsGeometry? padding;
+
+  final BoxDecoration? decoration;
+
+  final Widget? child;
+
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return alignment != null
+        ? Align(
+            alignment: alignment ?? Alignment.center, child: iconButtonWidget)
+        : iconButtonWidget;
+  }
+
+  Widget get iconButtonWidget => SizedBox(
+        height: height ?? 0,
+        width: width ?? 0,
+        child: IconButton(
+          padding: EdgeInsets.zero,
+          icon: Container(
+            height: height ?? 0,
+            width: width ?? 0,
+            padding: padding ?? EdgeInsets.zero,
+            decoration: decoration ??
+                BoxDecoration(
+                  color: theme.colorScheme.onErrorContainer,
+                  borderRadius: BorderRadius.circular(14.h),
+                ),
+            child: child,
+          ),
+          onPressed: onTap,
+        ),
+      );
+}
