c1e6756e60f66566d6a8a296d84251cd4d8b6972
dehkai <ydk1421@gmail.com>
2024-04-16 23:15:53 +0800

    FireBase Connection Configuration

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'core/app_export.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   ThemeHelper().changeTheme('primary');
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
+  //Check if the connection to Firebase was successful
+  if (Firebase.apps.isNotEmpty) {
+    print('Firebase connection established!');
+  } else {
+    print('Failed to connect to Firebase.');
+  }
   runApp(MyApp());
 }
+
+Future<bool> checkFirebaseConnection() async {
+  try {
+    await FirebaseFirestore.instance.collection('dummy').doc('dummy').get();
+    // If the code reaches here, the connection was successful
+    return true;
+  } catch (e) {
+    // Connection failed
+    return false;
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   @override
