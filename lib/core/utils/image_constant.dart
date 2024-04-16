9fbff4155c5ef860423025d23a4aaa6648806cf8
tancheesen123 <tancheesen123@hotmail.com>
2024-04-16 21:56:37 +0800

    WiseWork

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,89 @@
+// ignore_for_file: must_be_immutable
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+// Log In images
+  static String imgLocation = '$imagePath/img_location.svg';
+
+// Sign Up images
+  static String imgArrowLeft = '$imagePath/img_arrow_left.svg';
+
+  static String imgLock = '$imagePath/img_lock.svg';
+
+  static String imgLocationGray700 = '$imagePath/img_location_gray_700.svg';
+
+// Select Job Category images
+  static String imgPhBagFill = '$imagePath/img_ph_bag_fill.svg';
+
+  static String imgIcBaselinePeople = '$imagePath/img_ic_baseline_people.svg';
+
+// Select Job Preference images
+  static String imgMdiShopOutline = '$imagePath/img_mdi_shop_outline.svg';
+
+  static String imgLucideMusic = '$imagePath/img_lucide_music.svg';
+
+  static String imgIcOutlineWarehouse =
+      '$imagePath/img_ic_outline_warehouse.svg';
+
+  static String imgSearch = '$imagePath/img_search.svg';
+
+  static String imgBxDish = '$imagePath/img_bx_dish.svg';
+
+  static String imgFluentEmojiHi = '$imagePath/img_fluent_emoji_hi.svg';
+
+  static String imgIconParkOutli = '$imagePath/img_icon_park_outli.svg';
+
+  static String imgMdiCarOutline = '$imagePath/img_mdi_car_outline.svg';
+
+// Home - Container images
+  static String imgHome = '$imagePath/img_home.svg';
+
+  static String imgUserOnprimarycontainer =
+      '$imagePath/img_user_onprimarycontainer.svg';
+
+  static String imgPhBagFillOnprimarycontainer =
+      '$imagePath/img_ph_bag_fill_onprimarycontainer.svg';
+
+  static String imgHomeOnprimarycontainer =
+      '$imagePath/img_home_onprimarycontainer.svg';
+
+  static String imgNavSettings = '$imagePath/img_nav_settings.svg';
+
+// Home images
+  static String imgRectangle382 = '$imagePath/img_rectangle_382.png';
+
+  static String imgUser = '$imagePath/img_user.svg';
+
+  static String imgRectangle515 = '$imagePath/img_rectangle_515.png';
+
+  static String imgFavorite = '$imagePath/img_favorite.svg';
+
+  static String imgRectangle51540x40 = '$imagePath/img_rectangle_515_40x40.png';
+
+  static String imgRectangle5151 = '$imagePath/img_rectangle_515_1.png';
+
+  static String imgRectangle5152 = '$imagePath/img_rectangle_515_2.png';
+
+  static String imgTelevision = '$imagePath/img_television.svg';
+
+  static String imgRectangle516 = '$imagePath/img_rectangle_516.png';
+
+  static String imgRectangle51650x50 = '$imagePath/img_rectangle_516_50x50.png';
+
+  static String imgRectangle5161 = '$imagePath/img_rectangle_516_1.png';
+
+  static String imgNavigation = '$imagePath/img_navigation.png';
+
+// Common images
+  static String imgMessage = '$imagePath/img_message.svg';
+
+  static String imgUnion = '$imagePath/img_union.svg';
+
+  static String img1421929991558096326 =
+      '$imagePath/img_1421929991558096326.svg';
+
+  static String imgFacebook = '$imagePath/img_facebook.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
