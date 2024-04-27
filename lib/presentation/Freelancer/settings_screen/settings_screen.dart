aabd5a88971303702db9957f3a9ee5c47c7a0f0c
dehkai <ydk1421@gmail.com>
2024-04-27 15:54:52 +0800

    Revamp Navigation Bar

--- lib/presentation/Freelancer/settings_screen/settings_screen.dart
+++ lib/presentation/Freelancer/settings_screen/settings_screen.dart
@@ -352,10 +352,10 @@ class SettingsScreen extends StatelessWidget {
   String getCurrentRoute(BottomBarEnum type) {
     switch (type) {
       case BottomBarEnum.Home:
-        return AppRoutes.myjobApplicationsPage;
+        return AppRoutes.homeContainerScreen;
       case BottomBarEnum.Messages:
         return "/";
-      case BottomBarEnum.Myjobs:
+      case BottomBarEnum.Jobs:
         return "/";
       case BottomBarEnum.Notifications:
         return "/";
