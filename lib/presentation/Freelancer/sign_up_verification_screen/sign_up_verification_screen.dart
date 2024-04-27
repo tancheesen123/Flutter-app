3521d34ea8f5a7277e6f27b86027acae7d3b2bff
dehkai <ydk1421@gmail.com>
2024-04-27 22:13:59 +0800

    SignUp Email Verification

--- lib/presentation/Freelancer/sign_up_verification_screen/sign_up_verification_screen.dart
+++ lib/presentation/Freelancer/sign_up_verification_screen/sign_up_verification_screen.dart
@@ -1,15 +1,48 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../../../core/app_export.dart';
 import '../../../theme/custom_button_style.dart';
 import '../../../widgets/app_bar/appbar_leading_image.dart';
 import '../../../widgets/app_bar/custom_app_bar.dart';
 import '../../../widgets/custom_elevated_button.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class SignUpVerificationScreen extends StatelessWidget {
-  const SignUpVerificationScreen({Key? key})
-      : super(
-          key: key,
-        );
+class SignUpVerificationScreen extends StatefulWidget {
+  const SignUpVerificationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpVerificationScreen> createState() =>
+      _SignUpVerificationScreenState();
+}
+
+class _SignUpVerificationScreenState extends State<SignUpVerificationScreen> {
+  bool isVerified = false;
+  Timer? timer;
+  bool canResendEmail = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkVerificationStatus();
+    timer = Timer.periodic(
+        const Duration(seconds: 3), (timer) => checkVerificationStatus());
+    isVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+
+    if (!isVerified) {
+      sendVerificationEmail();
+      Timer.periodic(
+        const Duration(seconds: 3),
+        (timer) => checkVerificationStatus(),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    timer!.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +66,7 @@ class SignUpVerificationScreen extends StatelessWidget {
                 width: 306.h,
                 margin: EdgeInsets.symmetric(horizontal: 15.h),
                 child: Text(
-                  "An email has been send to your email for verification. Once verified, you’ ll be able to login.",
+                  "An email has been send to your email for verification. Once verified, you’ ll be redirect to home page.",
                   maxLines: 3,
                   overflow: TextOverflow.ellipsis,
                   textAlign: TextAlign.center,
@@ -51,8 +84,22 @@ class SignUpVerificationScreen extends StatelessWidget {
               SizedBox(height: 67.v),
               CustomElevatedButton(
                 height: 54.v,
-                text: "GO TO LOGIN",
+                text: "RESENT EMAIL",
                 buttonStyle: CustomButtonStyles.fillSecondaryContainerTL12,
+                onPressed: () {
+                  if (canResendEmail) {
+                    sendVerificationEmail();
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content:
+                            Text("Email can only be resent every 60 seconds"),
+                        duration: Duration(seconds: 2),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  }
+                },
               ),
               SizedBox(height: 5.v)
             ],
@@ -79,5 +126,31 @@ class SignUpVerificationScreen extends StatelessWidget {
   /// Navigates back to the previous screen.
   onTapArrowleftone(BuildContext context) {
     Navigator.pop(context);
+  }
+
+  Future sendVerificationEmail() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser!;
+      await user.sendEmailVerification();
+
+      setState(() {
+        canResendEmail = false;
+      });
+      await Future.delayed(Duration(seconds: 60));
+      setState(() {
+        canResendEmail = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future checkVerificationStatus() async {
+    await FirebaseAuth.instance.currentUser!.reload();
+    setState(() {
+      isVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+    });
+    if (isVerified) Navigator.pushNamed(context, AppRoutes.homeContainerScreen);
+    timer?.cancel();
   }
 }
