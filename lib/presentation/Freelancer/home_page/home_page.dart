f85630fa2aebcc07807859231ba476a98ad46c36
tancheesen123 <tancheesen123@hotmail.com>
2024-04-28 20:09:37 +0800

    update setting page

--- lib/presentation/Freelancer/home_page/home_page.dart
+++ lib/presentation/Freelancer/home_page/home_page.dart
@@ -10,12 +10,17 @@ import '../profile_screen/profile_screen.dart';
 // ignore_for_file: must_be_immutable
 
 // ignore_for_file: must_be_immutable
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({Key? key})
       : super(
           key: key,
         );
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   TextEditingController searchController = TextEditingController();
 
   @override
