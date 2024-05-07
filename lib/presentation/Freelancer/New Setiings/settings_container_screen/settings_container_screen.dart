7c75a3ca7cc9e8adca88d14bbd1dfc7da7efa907
dehkai <ydk1421@gmail.com>
2024-05-07 21:50:25 +0800

    Update UI

---
+++ lib/presentation/Freelancer/New Setiings/settings_container_screen/settings_container_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import '../../../../core/app_export.dart';
+import '../../../../widgets/custom_bottom_bar.dart';
+import '../settings_page/settings_page.dart'; // ignore_for_file: must_be_immutable
+
+// ignore_for_file: must_be_immutable
+class SettingsContainerScreen extends StatelessWidget {
+  SettingsContainerScreen({Key? key})
+      : super(
+          key: key,
+        );
+
+  GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Navigator(
+          key: navigatorKey,
+          initialRoute: AppRoutes.settingsPage,
+          onGenerateRoute: (routeSetting) => PageRouteBuilder(
+            pageBuilder: (ctx, ani, ani1) => getCurrentPage(routeSetting.name!),
+            transitionDuration: Duration(seconds: 0),
+          ),
+        ),
+        bottomNavigationBar: _buildBottomBar(context),
+      ),
+    );
+  }
+
+  /// Section Widget
+  Widget _buildBottomBar(BuildContext context) {
+    return CustomBottomBar(
+      onChanged: (BottomBarEnum type) {
+        Navigator.pushNamed(
+            navigatorKey.currentContext!, getCurrentRoute(type));
+      },
+    );
+  }
+
+  ///Handling route based on bottom click actions
+  String getCurrentRoute(BottomBarEnum type) {
+    switch (type) {
+      case BottomBarEnum.Home:
+        return AppRoutes.homeContainerScreen;
+      case BottomBarEnum.Jobs:
+        return "/";
+      case BottomBarEnum.Notifications:
+        return "/";
+      case BottomBarEnum.Settings:
+        return "/";
+      default:
+        return "/";
+    }
+  }
+
+  ///Handling page based on route
+  Widget getCurrentPage(String currentRoute) {
+    switch (currentRoute) {
+      case AppRoutes.settingsPage:
+        return SettingsPage();
+      default:
+        return DefaultWidget();
+    }
+  }
+}
