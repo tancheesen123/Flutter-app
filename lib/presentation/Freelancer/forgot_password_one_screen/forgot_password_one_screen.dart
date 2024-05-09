76dadbcf1fe5837e35f2b0e61a14dafecf3b0d52
dehkai <ydk1421@gmail.com>
2024-05-09 16:16:48 +0800

    SCRUM-16 Data validation

--- lib/presentation/Freelancer/forgot_password_one_screen/forgot_password_one_screen.dart
+++ lib/presentation/Freelancer/forgot_password_one_screen/forgot_password_one_screen.dart
@@ -32,6 +32,7 @@ class ForgotPasswordOneScreen extends StatelessWidget {
           ),
           child: Form(
             key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             child: Container(
               width: double.maxFinite,
               padding: EdgeInsets.symmetric(
@@ -68,36 +69,45 @@ class ForgotPasswordOneScreen extends StatelessWidget {
                   ),
                   SizedBox(height: 19.v),
                   Padding(
-                      padding: EdgeInsets.only(left: 6.h),
-                      child: Form(
-                        key: controller.ForgotPasswordFormKey,
-                        child: CustomTextFormField(
-                          controller: controller.email,
-                          hintText: "Email Address",
-                          textInputAction: TextInputAction.done,
-                          textInputType: TextInputType.emailAddress,
-                          prefix: Container(
-                            margin: EdgeInsets.fromLTRB(20.h, 15.v, 10.h, 15.v),
-                            child: CustomImageView(
-                              imagePath: ImageConstant.imgMessage,
-                              height: 24.adaptSize,
-                              width: 24.adaptSize,
+                    padding: EdgeInsets.only(left: 6.h),
+                    child: FutureBuilder<String?>(
+                      future: _validateEmail(controller),
+                      builder: (context, snapshot) {
+                        return Form(
+                          key: controller.ForgotPasswordFormKey,
+                          child: CustomTextFormField(
+                            controller: controller.email,
+                            hintText: "Email Address",
+                            textInputAction: TextInputAction.done,
+                            textInputType: TextInputType.emailAddress,
+                            prefix: Container(
+                              margin:
+                                  EdgeInsets.fromLTRB(20.h, 15.v, 10.h, 15.v),
+                              child: CustomImageView(
+                                imagePath: ImageConstant.imgMessage,
+                                height: 24.adaptSize,
+                                width: 24.adaptSize,
+                              ),
                             ),
+                            prefixConstraints: BoxConstraints(
+                              maxHeight: 54.v,
+                            ),
+                            contentPadding: EdgeInsets.only(
+                              top: 15.v,
+                              right: 30.h,
+                              bottom: 15.v,
+                            ),
+                            borderDecoration:
+                                TextFormFieldStyleHelper.fillWhiteATL12,
+                            textStyle: TextStyle(color: Colors.black),
+                            validator: (value) {
+                              return snapshot.data;
+                            },
                           ),
-                          prefixConstraints: BoxConstraints(
-                            maxHeight: 54.v,
-                          ),
-                          contentPadding: EdgeInsets.only(
-                            top: 15.v,
-                            right: 30.h,
-                            bottom: 15.v,
-                          ),
-                          borderDecoration:
-                              TextFormFieldStyleHelper.fillWhiteATL12,
-                          textStyle: TextStyle(color: Colors.black),
-                          
-                        ),
-                      )),
+                        );
+                      },
+                    ),
+                  ),
                   SizedBox(height: 34.v),
                   CustomElevatedButton(
                     height: 54.v,
@@ -105,7 +115,7 @@ class ForgotPasswordOneScreen extends StatelessWidget {
                     margin: EdgeInsets.only(left: 6.h),
                     buttonStyle: CustomButtonStyles.fillPrimaryTL12,
                     buttonTextStyle: CustomTextStyles.titleMediumWhiteA700,
-                    onPressed:() =>controller.sendPasswordResetEmail(context),
+                    onPressed: () => controller.sendPasswordResetEmail(context),
                   ),
                   SizedBox(height: 5.v)
                 ],
@@ -136,5 +146,19 @@ class ForgotPasswordOneScreen extends StatelessWidget {
     Navigator.pop(context);
   }
 
-  
+  Future<String?> _validateEmail(ForgotPasswordController controller) async {
+    String? value = controller.email.text;
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    } else if (!GetUtils.isEmail(value)) {
+      return 'Enter a valid email';
+    } else {
+      // Perform the asynchronous check if the email is registered
+      bool isRegistered = await controller.isEmailRegistered(value);
+      if (!isRegistered) {
+        return 'Email is not registered';
+      }
+    }
+    return null; // Return null if validation succeeds
+  }
 }
