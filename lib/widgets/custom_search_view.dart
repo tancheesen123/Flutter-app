5a15ebb06a5fc239e1c36a8ac22892a933bd192c
dehkai <ydk1421@gmail.com>
2024-05-31 20:33:17 +0800

    SCRUM-10 Change Search Bar to button

--- lib/widgets/custom_search_view.dart
+++ lib/widgets/custom_search_view.dart
@@ -2,116 +2,100 @@ import 'package:flutter/material.dart';
 import '../core/app_export.dart';
 
 class CustomSearchView extends StatelessWidget {
-  CustomSearchView(
-      {Key? key,
-      this.alignment,
-      this.width,
-      this.scrollPadding,
-      this.controller,
-      this.focusNode,
-      this.autofocus = false,
-      this.textStyle,
-      this.textInputType = TextInputType.text,
-      this.maxLines,
-      this.hintText,
-      this.hintStyle,
-      this.prefix,
-      this.prefixConstraints,
-      this.suffix,
-      this.suffixConstraints,
-      this.contentPadding,
-      this.borderDecoration,
-      this.fillColor,
-      this.filled = true,
-      this.validator,
-      this.onChanged})
-      : super(
+  CustomSearchView({
+    Key? key,
+    this.alignment,
+    this.width,
+    this.scrollPadding,
+    this.controller,
+    this.focusNode,
+    this.autofocus = false,
+    this.textStyle,
+    this.textInputType = TextInputType.text,
+    this.maxLines,
+    this.hintText,
+    this.hintStyle,
+    this.prefix,
+    this.prefixConstraints,
+    this.suffix,
+    this.suffixConstraints,
+    this.contentPadding,
+    this.borderDecoration,
+    this.fillColor,
+    this.filled = true,
+    this.validator,
+    this.onChanged,
+    this.onTap,
+  }) : super(
           key: key,
         );
 
   final Alignment? alignment;
-
   final double? width;
-
   final TextEditingController? scrollPadding;
-
   final TextEditingController? controller;
-
   final FocusNode? focusNode;
-
   final bool? autofocus;
-
   final TextStyle? textStyle;
-
   final TextInputType? textInputType;
-
   final int? maxLines;
-
   final String? hintText;
-
   final TextStyle? hintStyle;
-
   final Widget? prefix;
-
   final BoxConstraints? prefixConstraints;
-
   final Widget? suffix;
-
   final BoxConstraints? suffixConstraints;
-
   final EdgeInsets? contentPadding;
-
   final InputBorder? borderDecoration;
-
   final Color? fillColor;
-
   final bool? filled;
-
   final FormFieldValidator<String>? validator;
-
   final Function(String)? onChanged;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return alignment != null
         ? Align(
             alignment: alignment ?? Alignment.center,
-            child: searchViewWidget(context))
+            child: searchViewWidget(context),
+          )
         : searchViewWidget(context);
   }
 
   Widget searchViewWidget(BuildContext context) => SizedBox(
         width: width ?? double.maxFinite,
-        child: TextFormField(
-          scrollPadding:
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          controller: controller,
-          focusNode: focusNode,
-          onTapOutside: (event) {
-            if (focusNode != null) {
-              focusNode?.unfocus();
-            } else {
-              FocusManager.instance.primaryFocus?.unfocus();
-            }
-          },
-          autofocus: autofocus!,
-          style: textStyle ?? theme.textTheme.bodyLarge,
-          keyboardType: textInputType,
-          maxLines: maxLines ?? 1,
-          decoration: decoration,
-          validator: validator,
-          onChanged: (String value) {
-            onChanged?.call(value);
-          },
+        child: GestureDetector(
+          onTap: onTap,
+          child: TextFormField(
+            scrollPadding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+            controller: controller,
+            focusNode: focusNode,
+            onTapOutside: (event) {
+              if (focusNode != null) {
+                focusNode?.unfocus();
+              } else {
+                FocusManager.instance.primaryFocus?.unfocus();
+              }
+            },
+            autofocus: autofocus!,
+            style: textStyle ?? theme.textTheme.bodyLarge,
+            keyboardType: textInputType,
+            maxLines: maxLines ?? 1,
+            decoration: decoration,
+            validator: validator,
+            onChanged: (String value) {
+              onChanged?.call(value);
+            },
+          ),
         ),
       );
+
   InputDecoration get decoration => InputDecoration(
         hintText: hintText ?? "",
         hintStyle: hintStyle ?? theme.textTheme.bodyLarge,
         prefixIcon: Padding(
-          padding: EdgeInsets.all(
-            15.h,
-          ),
+          padding: EdgeInsets.all(15.h),
           child: Icon(
             Icons.search,
             color: Colors.grey.shade600,
