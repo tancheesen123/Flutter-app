52c826d4fb30ed16545e64cd862fe1e95a1a3d88
dehkai <ydk1421@gmail.com>
2024-06-01 17:15:51 +0800

    SCRUM-10 Search

--- lib/presentation/Freelancer/search_tab_container_screen/search_tab_container_screen.dart
+++ lib/presentation/Freelancer/search_tab_container_screen/search_tab_container_screen.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../Controller/SearchPageController.dart';
 import '../../../core/app_export.dart';
 import '../../../widgets/app_bar/appbar_leading_image.dart';
 import '../../../widgets/app_bar/appbar_title.dart';
 import '../../../widgets/app_bar/custom_app_bar.dart';
 import '../../../widgets/custom_icon_button.dart';
 import '../../../widgets/custom_search_view.dart';
-import '../search_page/search_page.dart';
+import '../applyjob/apply_job_page.dart';
 
 class SearchTabContainerScreen extends StatefulWidget {
-  const SearchTabContainerScreen({Key? key})
-      : super(
-          key: key,
-        );
+  const SearchTabContainerScreen({Key? key}) : super(key: key);
 
   @override
   SearchTabContainerScreenState createState() =>
       SearchTabContainerScreenState();
 }
-// ignore_for_file: must_be_immutable
+
 class SearchTabContainerScreenState extends State<SearchTabContainerScreen>
     with TickerProviderStateMixin {
   TextEditingController searchController = TextEditingController();
+  final SearchPageController searchPageController =
+      Get.put(SearchPageController());
 
   late TabController tabviewController;
 
@@ -34,101 +35,52 @@ class SearchTabContainerScreenState extends State<SearchTabContainerScreen>
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        resizeToAvoidBottomInset: false,
+        resizeToAvoidBottomInset: true,
         appBar: _buildAppBar(context),
-        body: SizedBox(
-          width: double.maxFinite,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 11.v),
-              _buildSearchBox(context),
-              SizedBox(height: 19.v),
-              Padding(
-                padding: EdgeInsets.only(left: 20.h),
-                child: Text(
-                  "35 Job Opportunity",
-                  style: CustomTextStyles.bodyLargePrimaryContainer,
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: double.maxFinite,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 11.v),
+                _buildSearchBox(context),
+                SizedBox(height: 19.v),
+                Padding(
+                  padding: EdgeInsets.only(left: 20.h),
+                  child: Obx(() {
+                    int jobCount = searchPageController.searchResults.length;
+                    return Text(
+                      "$jobCount Job Opportunit${jobCount == 1 || jobCount == 0 ? 'y' : 'ies'}",
+                      style: CustomTextStyles.bodyLargePrimaryContainer,
+                    );
+                  }),
                 ),
-              ),
-              SizedBox(height: 12.v),
-              Container(
-                height: 50.v,
-                width: 295.h,
-                margin: EdgeInsets.only(left: 20.h),
-                child: TabBar(
-                  controller: tabviewController,
-                  labelPadding: EdgeInsets.zero,
-                  labelColor: appTheme.gray700,
-                  labelStyle: TextStyle(
-                    fontSize: 14.fSize,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                  ),
-                  unselectedLabelColor: appTheme.whiteA700,
-                  unselectedLabelStyle: TextStyle(
-                    fontSize: 14.fSize,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                  ),
-                  indicatorSize: TabBarIndicatorSize.tab,
-                  indicator: BoxDecoration(
-                    color: appTheme.gray5002,
-                    borderRadius: BorderRadius.circular(
-                      12.h,
-                    ),
-                  ),
-                  tabs: [
-                    Tab(
-                      child: Text(
-                        "Most Relevent",
-                      ),
-                    ),
-                    Tab(
-                      child: Text(
-                        "Most Recent",
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 557.v,
-                child: TabBarView(
-                  controller: tabviewController,
-                  children: [SearchPage(), SearchPage()],
-                ),
-              )
-            ],
+                SizedBox(height: 12.v),
+                buildSearchedJobs(context),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  /// Section Widget
   PreferredSizeWidget _buildAppBar(BuildContext context) {
     return CustomAppBar(
       leadingWidth: 30.h,
       leading: AppbarLeadingImage(
         imagePath: ImageConstant.imgArrowLeft,
-        margin: EdgeInsets.only(
-          left: 20.h,
-          top: 18.v,
-          bottom: 16.v,
-        ),
+        margin: EdgeInsets.only(left: 20.h, top: 18.v, bottom: 16.v),
         onTap: () {
           onTapArrowleftone(context);
         },
       ),
       centerTitle: true,
-      title: AppbarTitle(
-        text: "Search",
-      ),
+      title: AppbarTitle(text: "Search"),
     );
   }
 
-  /// Section Widget
   Widget _buildSearchBox(BuildContext context) {
     return Align(
       alignment: Alignment.center,
@@ -140,7 +92,16 @@ class SearchTabContainerScreenState extends State<SearchTabContainerScreen>
             Expanded(
               child: CustomSearchView(
                 controller: searchController,
-                hintText: "Warehouse Crew",
+                hintText: "Search Here...",
+                onChanged: (value) {
+                  if (value.isNotEmpty) {
+                    searchPageController.searchJobPosts(value);
+                  } else {
+                    // Clear search results when search text is empty
+                    searchPageController.searchResults.clear();
+                  }
+                },
+                textStyle: TextStyle(color: Colors.black),
               ),
             ),
             Padding(
@@ -154,15 +115,137 @@ class SearchTabContainerScreenState extends State<SearchTabContainerScreen>
                   imagePath: ImageConstant.imgUser,
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
 
-  /// Navigates back to the previous screen.
-  onTapArrowleftone(BuildContext context) {
+  Widget buildSearchedJobs(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.h),
+      child: Obx(() {
+        if (searchPageController.searchResults.isEmpty) {
+          return Center(child: Text('No results found'));
+        } else {
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: searchPageController.searchResults.length,
+            itemBuilder: (context, index) {
+              final data = searchPageController.searchResults[index].data()
+                  as Map<String, dynamic>;
+              return GestureDetector(
+                onTap: () {
+                  String postId = data['postId'];
+                  Navigator.of(context, rootNavigator: true).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return ApplyJobScreen(
+                          postId: postId,
+                        );
+                      },
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 12.v),
+                      decoration: AppDecoration.outlineGray.copyWith(
+                        borderRadius: BorderRadiusStyle.roundedBorder20,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomImageView(
+                            imagePath: ImageConstant.imgRectangle516,
+                            height: 50.adaptSize,
+                            width: 50.adaptSize,
+                            radius: BorderRadius.circular(15.h),
+                            margin: EdgeInsets.symmetric(vertical: 13.v),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 5.v),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: 233.h,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                            top: 6.v, bottom: 4.v),
+                                        child: Text(
+                                          data['title'] ?? '',
+                                          style: theme.textTheme.bodySmall,
+                                        ),
+                                      ),
+                                      CustomIconButton(
+                                        height: 28.adaptSize,
+                                        width: 28.adaptSize,
+                                        padding: EdgeInsets.all(6.h),
+                                        child: CustomImageView(
+                                          imagePath: ImageConstant.imgFavorite,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Text(
+                                  data['title'] ?? 'Warehouse Crew',
+                                  style: theme.textTheme.titleMedium,
+                                ),
+                                SizedBox(height: 4.v),
+                                SizedBox(
+                                  width: 231.h,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        data['budget'] != null
+                                            ? 'RM${data['budget']}/12h'
+                                            : '',
+                                        style: theme.textTheme.bodySmall,
+                                      ),
+                                      Text(
+                                        data['location'] ?? '',
+                                        style: theme.textTheme.bodySmall,
+                                      ),
+                                      Text(
+                                        data['workingHours'] != null
+                                            ? '${data['workingHours']}h'
+                                            : '',
+                                        style: theme.textTheme.labelLarge,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 15.v), // Add spacing between items
+                  ],
+                ),
+              );
+            },
+          );
+        }
+      }),
+    );
+  }
+
+  void onTapArrowleftone(BuildContext context) {
+    searchPageController.searchResults.clear();
     Navigator.pop(context);
   }
 }
