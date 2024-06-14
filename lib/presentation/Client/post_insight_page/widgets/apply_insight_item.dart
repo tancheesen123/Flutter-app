66f0006164b5de75a2882de7584b81fe4d6e5fbe
tancheesen123 <tancheesen123@hotmail.com>
2024-06-14 19:11:06 +0800

    add company detail for client and freelancer ui

--- lib/presentation/Client/post_insight_page/widgets/apply_insight_item.dart
+++ lib/presentation/Client/post_insight_page/widgets/apply_insight_item.dart
@@ -24,44 +24,61 @@ class _ApplyInsightLineChartState extends State<ApplyInsightLineChart> {
   final PostInsightController postInsightController =
       Get.put(PostInsightController());
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('7-Day Apply Bar Chart'),
-      ),
-      body: FutureBuilder<List<Map<String, dynamic>>>(
-        future:
-            postInsightController.getLast7DaysApplyData(widget.postInsightData),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text("Error loading clicks data"));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text("No clicks data available"));
-          } else {
-            List<Map<String, dynamic>> clickData = snapshot.data!;
-            List<SalesData> salesData = clickData.map((data) {
-              return SalesData(data['date'], data['value'].toDouble());
-            }).toList();
-            return Center(
-              child: SfCartesianChart(
-                isTransposed: true,
-                primaryXAxis: CategoryAxis(),
-                primaryYAxis: NumericAxis(),
-                series: <BarSeries<SalesData, String>>[
-                  BarSeries<SalesData, String>(
-                    dataSource: salesData,
-                    xValueMapper: (SalesData sales, _) => sales.month,
-                    yValueMapper: (SalesData sales, _) => sales.sales,
-                    dataLabelSettings: DataLabelSettings(isVisible: true),
-                  ),
-                ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Title Section
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                '7-Day Apply Bar Chart',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            );
-          }
-        },
+            ),
+            // Chart Section
+            Expanded(
+              child: FutureBuilder<List<Map<String, dynamic>>>(
+                future: postInsightController
+                    .getLast7DaysApplyData(widget.postInsightData),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text("Error loading clicks data"));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(child: Text("No clicks data available"));
+                  } else {
+                    List<Map<String, dynamic>> clickData = snapshot.data!;
+                    List<SalesData> salesData = clickData.map((data) {
+                      return SalesData(data['date'], data['value'].toDouble());
+                    }).toList();
+                    return Center(
+                      child: SfCartesianChart(
+                        isTransposed: true,
+                        primaryXAxis: CategoryAxis(),
+                        primaryYAxis: NumericAxis(),
+                        series: <BarSeries<SalesData, String>>[
+                          BarSeries<SalesData, String>(
+                            dataSource: salesData,
+                            xValueMapper: (SalesData sales, _) => sales.month,
+                            yValueMapper: (SalesData sales, _) => sales.sales,
+                            dataLabelSettings:
+                                DataLabelSettings(isVisible: true),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
