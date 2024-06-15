07e47ec925c1ba0814ce2ed2eca5b572fe3f0212
mohamadazafri <azafri9@gmail.com>
2024-06-15 17:28:48 +0800

    Update candidate page

--- lib/Controller/CandidateController.dart
+++ lib/Controller/CandidateController.dart
@@ -1,9 +1,5 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:workwise/Controller/NotificationController.dart';
 
 class CandidateController extends GetxController {
@@ -14,25 +10,66 @@ class CandidateController extends GetxController {
   var candidates = <Map<String, dynamic>>[].obs;
   var email = ''.obs;
   var deviceToken = ''.obs;
-  final NotificationController notificationController =
-      Get.put(NotificationController());
+  final NotificationController notificationController = Get.put(NotificationController());
 
-  Future<bool> acceptCandidate(
-      dynamic postDetail, List listAcceptedCandidate) async {
+  Future<bool> acceptCandidate(dynamic postDetail, List listAcceptedCandidate, bool stopAcceptCandidate) async {
+    bool success = false;
     try {
-      // await FirebaseFirestore.instance.collection('jobPost').doc(postDetail["id"]).update({"postStatus": "EMPLOYED"});
       for (var doc in listAcceptedCandidate) {
         print("this is doc's user ${doc["detail"]["user"]}");
 
         DocumentSnapshot userData = await getData(doc["detail"]["user"]);
-
-        // Perform your notification handling or any other logic here
 
         notificationController.sendNotification(
             "${userData["Token"]}",
             "Your application for ${postDetail["data"]["title"]} has been accepted",
             "Congratulations! You have been accepted for the job ${postDetail["data"]["title"]} your Next step are:\n${postDetail["data"]["description"]} ",
             userData["email"]);
+      }
+
+      if (stopAcceptCandidate) {
+        FirebaseFirestore.instance.collection('jobPost').doc(postDetail["id"]).update({"postStatus": "EMPLOYED"});
+
+        List acceptedUserRefList = [];
+
+        listAcceptedCandidate.forEach((acceptedCandidate) {
+          acceptedUserRefList.add(acceptedCandidate["detail"]["user"]);
+        });
+
+        await FirebaseFirestore.instance
+            .collection('jobPost')
+            .doc(postDetail["id"])
+            .collection("candidate")
+            .where("user", whereNotIn: acceptedUserRefList)
+            .get()
+            .then((value) async {
+          List rejectUser = value.docs;
+
+          await Future.forEach<dynamic>(rejectUser, (candidate) async {
+            FirebaseFirestore.instance
+                .collection('jobPost')
+                .doc(postDetail["id"])
+                .collection("candidate")
+                .doc(candidate.id)
+                .update({"status": "Reject"});
+          });
+
+          await Future.forEach<dynamic>(rejectUser, (candidate) async {
+            dynamic userApplication = await FirebaseFirestore.instance
+                .collection('user')
+                .doc(candidate.id)
+                .collection("Application")
+                .where("postRefPath", isEqualTo: postDetail["postReference"].reference)
+                .get();
+
+            await FirebaseFirestore.instance
+                .collection('user')
+                .doc(candidate.id)
+                .collection("Application")
+                .doc(userApplication.docs[0].id)
+                .update({"status": "Reject"});
+          });
+        });
       }
 
       await Future.forEach<dynamic>(listAcceptedCandidate, (candidate) async {
@@ -49,11 +86,9 @@ class CandidateController extends GetxController {
             .collection('user')
             .doc(candidate["id"])
             .collection("Application")
-            .where("postRefPath",
-                isEqualTo: postDetail["postReference"].reference)
+            .where("postRefPath", isEqualTo: postDetail["postReference"].reference)
             .get();
 
-        print(userApplication);
         await FirebaseFirestore.instance
             .collection('user')
             .doc(candidate["id"])
@@ -61,10 +96,13 @@ class CandidateController extends GetxController {
             .doc(userApplication.docs[0].id)
             .update({"status": "Accept"});
       });
-      return true;
+
+      success = true;
     } catch (e) {
-      return false;
+      success = false;
     }
+
+    return success;
   }
 
   Future<DocumentSnapshot> getData(DocumentReference userRef) async {
@@ -77,28 +115,4 @@ class CandidateController extends GetxController {
       rethrow;
     }
   }
-
-  // Future<QuerySnapshot> fetchNotificationData(dynamic userData) async {
-  //   try {
-  //     print("This is userData: ${userData["email"]}");
-
-  //     QuerySnapshot notificationSnapshot = await FirebaseFirestore.instance
-  //         .collection("user")
-  //         .doc(userData["email"])
-  //         .collection("Notification")
-  //         .get();
-
-  //     // Loop through each document in the snapshot
-  //     notificationSnapshot.docs.forEach((doc) {
-  //       // Access document data using doc.data()
-  //       print("Document ID: ${doc.id}");
-  //       print("Document Data: ${doc.data()}");
-  //     });
-
-  //     return notificationSnapshot;
-  //   } catch (e) {
-  //     print("Error fetching notification data: $e");
-  //     throw e; // Optionally rethrow the error to handle it higher up in the call stack
-  //   }
-  // }
 }
